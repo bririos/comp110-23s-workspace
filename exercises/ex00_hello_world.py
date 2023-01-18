8041ce805dcc6903a07948190555c5f98f0324ea
Briseyda Rios <briseydarios4@gmail.com>
2023-01-18 14:30:25 -0500

    "Finished Exercise 00!"

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,6 +1,5 @@
-""""My first program for COMP110."""
-
-__author__="7730576067"
+"""My first program for COMP110."""
+__author__ = "7730576067"
 print("Hello, world.")
 print("Hello,to everyone in the world")
 print("Hello, humans in the world.")
